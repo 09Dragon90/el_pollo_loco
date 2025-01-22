ce0f9491e0d3a1707d08db6bbf8f1708082cf570
Niko Helwig <niko.helwig@stickerei-helwig.de>
2025-01-22 21:04:34 +0100

    hitbox changed

--- classes/endboss.class.js
+++ classes/endboss.class.js
@@ -3,6 +3,12 @@ class Endboss extends MoveableObject {
   width = 250;
 
   imagesWalking = [
+    "assets/img/4_enemie_boss_chicken/1_walk/G1.png",
+    "assets/img/4_enemie_boss_chicken/1_walk/G2.png",
+    "assets/img/4_enemie_boss_chicken/1_walk/G3.png",
+    "assets/img/4_enemie_boss_chicken/1_walk/G4.png",
+  ];
+  imagesAlert = [
     "assets/img/4_enemie_boss_chicken/2_alert/G5.png",
     "assets/img/4_enemie_boss_chicken/2_alert/G6.png",
     "assets/img/4_enemie_boss_chicken/2_alert/G7.png",
@@ -12,14 +18,38 @@ class Endboss extends MoveableObject {
     "assets/img/4_enemie_boss_chicken/2_alert/G11.png",
     "assets/img/4_enemie_boss_chicken/2_alert/G12.png",
   ];
+  imagesAttack = [
+    "assets/img/4_enemie_boss_chicken/3_attack/G13.png",
+    "assets/img/4_enemie_boss_chicken/3_attack/G14.png",
+    "assets/img/4_enemie_boss_chicken/3_attack/G15.png",
+    "assets/img/4_enemie_boss_chicken/3_attack/G16.png",
+    "assets/img/4_enemie_boss_chicken/3_attack/G17.png",
+    "assets/img/4_enemie_boss_chicken/3_attack/G18.png",
+    "assets/img/4_enemie_boss_chicken/3_attack/G19.png",
+    "assets/img/4_enemie_boss_chicken/3_attack/G20.png",
+  ];
+  imagesHurt = [
+    "assets/img/4_enemie_boss_chicken/4_hurt/G21.png",
+    "assets/img/4_enemie_boss_chicken/4_hurt/G22.png",
+    "assets/img/4_enemie_boss_chicken/4_hurt/G23.png",
+  ];
+  imagesDead = [
+    "assets/img/4_enemie_boss_chicken/5_dead/G24.png",
+    "assets/img/4_enemie_boss_chicken/5_dead/G25.png",
+    "assets/img/4_enemie_boss_chicken/5_dead/G26.png",
+  ];
 
   constructor(lengthOfLevel) {
     super();
     this.loadImg(this.imagesWalking[0]);
     this.loadImages(this.imagesWalking);
+    this.loadImages(this.imagesAlert);
+    this.loadImages(this.imagesAttack);
+    this.loadImages(this.imagesHurt);
+    this.loadImages(this.imagesDead);
     this.x = lengthOfLevel - this.width - 50;
     this.y = this.calY(this.height, this.overGroundY) + 10;
-    this.setHitbox(50, 10, 0, 0);
+    this.setHitbox(60, 10, 30, 10);
     this.animation();
   }
 
