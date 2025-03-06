7cacf96d0558d828795c7a834e899a2a5a074bfb
Niko Helwig <niko.helwig@stickerei-helwig.de>
2025-03-06 19:55:30 +0100

    Todos added

--- script.js
+++ script.js
@@ -16,3 +16,13 @@ function closeDialog(event) {
     dialogRef.close();
   }
 }
+
+// TODO
+// Impressum muss mit einem klick erreichbar sein
+// Mute in Local Storage speichern
+// Schaden am Pepe vergrößern
+// Nach Jump ist pepe nicht auf gleicher höhe
+// Nach Lose keine Buttons für replay
+// Buttens Steuerung bei Ipad schon einplenden
+// Buttons Kontext menue unterdrücken
+// Hitbox chickens kleiner machen
