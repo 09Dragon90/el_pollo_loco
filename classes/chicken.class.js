c7b050412129719ca0449fd7df4713de1088206b
NIKHEL <niko.helwig@stickerei-helwig.de>
2025-03-06 10:11:35 +0100

    code cleaning and JsDOC added

--- classes/chicken.class.js
+++ classes/chicken.class.js
@@ -21,17 +21,30 @@ class Chicken extends MoveableObject {
   constructor(lengthOfLevel, small) {
     super();
     this.small = small;
+    this.createdEnemy(lengthOfLevel);
+    this.speed = 0.15 + Math.random() * 0.5;
+    this.setHitbox(0, 0, 0, 0);
+    this.loadSounds();
+    this.animation();
+    this.movements();
+  }
+
+  /**
+   * Check if this a small or big enemy
+   * @param {number} lengthOfLevel - Numbers of level segments
+   */
+  createdEnemy(lengthOfLevel) {
     if (this.small) {
       this.createSmall(lengthOfLevel);
     } else {
       this.craeteBig(lengthOfLevel);
     }
-    this.speed = 0.15 + Math.random() * 0.5;
-    this.setHitbox(0, 0, 0, 0);
-    this.loadSounds();
-    this.animation();
   }
 
+  /**
+   * Created a big enemy
+   * @param {number} lengthOfLevel - Numbers of level segments
+   */
   craeteBig(lengthOfLevel) {
     this.loadImg(this.imagesWalking[0]);
     this.loadImages(this.imagesWalking);
@@ -39,6 +52,10 @@ class Chicken extends MoveableObject {
     this.y = this.calY(this.height, this.overGroundY);
   }
 
+  /**
+   * Created a small enemy
+   * @param {number} lengthOfLevel - Numbers of level segments
+   */
   createSmall(lengthOfLevel) {
     this.height = this.height / 1.5;
     this.width = this.width / 1.5;
@@ -48,16 +65,28 @@ class Chicken extends MoveableObject {
     this.y = this.calY(this.height, this.overGroundY);
   }
 
+  /**
+   * Load all Sounds
+   */
   loadSounds() {
     this.createdSound("assets/audio/hitChicken.mp3", "hit_sound");
   }
 
+  /**
+   * Play the animations
+   */
   animation() {
     this.stoppableInterval(
       setInterval(() => {
         this.moveLeft();
       }, 1000 / 60)
     );
+  }
+
+  /**
+   * Moving the Character
+   */
+  movements() {
     this.stoppableInterval(
       setInterval(() => {
         if (this.small) {
@@ -69,6 +98,9 @@ class Chicken extends MoveableObject {
     );
   }
 
+  /**
+   * If enemy hit delet hitbox and load dead img
+   */
   hit() {
     this.energy = 0;
     this.sounds.hit_sound.play();
