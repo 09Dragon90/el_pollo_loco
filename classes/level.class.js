0c72c17b95f52970972c75647f0725e24a363d77
NIKHEL <niko.helwig@stickerei-helwig.de>
2025-03-06 13:23:15 +0100

    code cleaning and JsDOC added

--- classes/level.class.js
+++ classes/level.class.js
@@ -6,6 +6,13 @@ class Level {
   lengthOfLevel;
   collectableItems = [];
 
+  backgroundImages = [
+    "assets/img/5_background/layers/air.png",
+    "assets/img/5_background/layers/3_third_layer/full.png",
+    "assets/img/5_background/layers/2_second_layer/full.png",
+    "assets/img/5_background/layers/1_first_layer/full.png",
+  ];
+
   constructor(
     numbersOfEnemies = 1,
     numbersOfEnemiesSmall = 1,
@@ -23,12 +30,22 @@ class Level {
     this.createBottles(numbersOfBottles, this.calLengthOfLevelItems());
   }
 
+  /**
+   * Created the coins object
+   * @param {number} numbers - Numbers of coins
+   * @param {number} lengthOfLevel - Numbers of level segments
+   */
   createCoins(numbers, lengthOfLevel) {
     for (let index = 0; index < numbers; index++) {
       this.collectableItems.push(new CollectableObject("coin", lengthOfLevel));
     }
   }
 
+  /**
+   * Created the bottle object
+   * @param {number} numbers - Numbers of bottle
+   * @param {number} lengthOfLevel - Numbers of level segments
+   */
   createBottles(numbers, lengthOfLevel) {
     for (let index = 0; index < numbers; index++) {
       this.collectableItems.push(
@@ -37,70 +54,64 @@ class Level {
     }
   }
 
+  /**
+   * Created the clouds object
+   * @param {number} numbers - Numbers of clouds
+   */
   createClouds(numbers) {
     for (let index = 0; index < numbers + 5; index++) {
       this.clouds.push(new Cloud(index * (this.width / 2)));
     }
   }
 
+  /**
+   * Calculate the length of level for collecteble items
+   * @returns - length of level for collecteble items
+   */
   calLengthOfLevelItems() {
     let indexEndboss = this.enemies.length - 1;
     return this.enemies[indexEndboss].x;
   }
 
+  /**
+   * Created the enimies object
+   * @param {number} numbers - Numbers of enimies
+   * @param {number} lengthOfLevel - Numbers of level segments
+   * @param {boolean} small - Size of enemy
+   */
   createEnemies(numbers, lengthOfLevel, small) {
     for (let index = 0; index < numbers; index++) {
       this.enemies.push(new Chicken(lengthOfLevel, small));
     }
   }
 
+  /**
+   * Created the endboss object
+   * @param {number} lengthOfLevel - Numbers of level segments
+   */
   createEndboss(lengthOfLevel) {
     this.enemies.push(new Endboss(lengthOfLevel));
   }
 
+  /**
+   * Created the level segments
+   * @param {number} numbers - Numbers of level segments
+   */
   createBackground(numbers) {
-    this.backgroundObjects = [
-      new BackgroundObject(
-        "assets/img/5_background/layers/air.png",
-        -this.width
-      ),
-      new BackgroundObject(
-        "assets/img/5_background/layers/3_third_layer/full.png",
-        -this.width
-      ),
-      new BackgroundObject(
-        "assets/img/5_background/layers/2_second_layer/full.png",
-        -this.width
-      ),
-      new BackgroundObject(
-        "assets/img/5_background/layers/1_first_layer/full.png",
-        -this.width
-      ),
-    ];
+    this.createBackgrounds(-1);
     for (let index = 0; index < numbers; index++) {
+      this.createBackgrounds(index);
+    }
+  }
+
+  /**
+   * Created the background images
+   * @param {number} index - Position of segment
+   */
+  createBackgrounds(index) {
+    for (let i = 0; i < this.backgroundImages.length; i++) {
       this.backgroundObjects.push(
-        new BackgroundObject(
-          "assets/img/5_background/layers/air.png",
-          this.width * index
-        )
-      );
-      this.backgroundObjects.push(
-        new BackgroundObject(
-          "assets/img/5_background/layers/3_third_layer/full.png",
-          this.width * index
-        )
-      );
-      this.backgroundObjects.push(
-        new BackgroundObject(
-          "assets/img/5_background/layers/2_second_layer/full.png",
-          this.width * index
-        )
-      );
-      this.backgroundObjects.push(
-        new BackgroundObject(
-          "assets/img/5_background/layers/1_first_layer/full.png",
-          this.width * index
-        )
+        new BackgroundObject(this.backgroundImages[i], this.width * index)
       );
     }
   }
