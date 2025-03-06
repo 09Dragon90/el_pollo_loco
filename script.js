8044df24b67e6575104679452977de0e711c4b7c
NIKHEL <niko.helwig@stickerei-helwig.de>
2025-03-06 14:12:54 +0100

    code cleaning and JsDOC added

--- script.js
+++ script.js
@@ -1,13 +1,18 @@
 let dialogRef = document.getElementById("dialog");
 
+/**
+ * Open the Dialog
+ */
 function openDialog() {
   dialogRef.showModal();
 }
 
+/**
+ * Close the Dialog
+ * @param {object} event - Click event
+ */
 function closeDialog(event) {
   if (event.target === dialogRef) {
-    // let bodyRef = document.getElementById("body");
-    // bodyRef.classList.remove("modal-open");
     dialogRef.close();
   }
 }
