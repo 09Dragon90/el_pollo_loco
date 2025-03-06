da67b4a73d0564d77a7e2f7ad2facec1dca264e5
NIKHEL <niko.helwig@stickerei-helwig.de>
2025-03-06 10:59:38 +0100

    code cleaning and JsDOC added

--- classes/drawable-object.class.js
+++ classes/drawable-object.class.js
@@ -11,17 +11,29 @@ class DrawableObject {
   isFlipped = false;
   sounds = {};
 
+  /**
+   * Created a Id for the Object
+   * with time and random number
+   */
   createdId() {
     const timestamp = Date.now();
     const randomNumber = Math.floor(Math.random() * 1e8);
     this.instanzId = `${timestamp}${randomNumber}`;
   }
 
+  /**
+   * Created a image elemet
+   * @param {string} path - Path of the image
+   */
   loadImg(path) {
     this.img = new Image();
     this.img.src = path;
   }
 
+  /**
+   * Created images from a array and save in the cache
+   * @param {Array} arr - Array of paths for images
+   */
   loadImages(arr) {
     arr.forEach((path) => {
       let img = new Image();
@@ -30,10 +42,19 @@ class DrawableObject {
     });
   }
 
+  /**
+   * Created a audio elemet
+   * @param {string} path - Path of the audio
+   * @param {string} name - Name of the audio
+   */
   createdSound(path, name) {
     this.sounds[name] = new Audio(path);
   }
 
+  /**
+   * Mute or unmute all audios in the array
+   * @param {boolean} status - Status auf mute
+   */
   muteSounds(status) {
     Object.values(this.sounds).forEach((sound) => {
       if (sound instanceof Audio) {
@@ -42,32 +63,18 @@ class DrawableObject {
     });
   }
 
-  // drawBorder(ctx) {
-  //   if (
-  //     this instanceof Character ||
-  //     this instanceof Chicken ||
-  //     this instanceof Endboss ||
-  //     this instanceof ThrowableObject ||
-  //     this instanceof CollectableObject
-  //   ) {
-  //     ctx.beginPath();
-  //     ctx.lineWidth = "4";
-  //     ctx.strokeStyle = "red";
-  //     ctx.rect(
-  //       this.hitbox.x,
-  //       this.hitbox.y,
-  //       this.hitbox.width,
-  //       this.hitbox.height
-  //     );
-  //     ctx.stroke();
-  //   }
-  // }
-
+  /**
+   * Saves the id of intevall in the array
+   * @param {Function} interval - Function of intervall
+   */
   stoppableInterval(interval) {
     let id = interval;
     this.intervalIds.push(id);
   }
 
+  /**
+   * Stopps all intervalls of object
+   */
   stopIntervals() {
     this.intervalIds.forEach((id) => {
       clearInterval(id);
@@ -75,6 +82,13 @@ class DrawableObject {
     this.intervalIds = [];
   }
 
+  /**
+   * Set the hitbox of object with the offset
+   * @param {Number} offsetYT - Number of offset for YT
+   * @param {Number} offsetYB - Number of offset for YB
+   * @param {Number} offsetXL - Number of offset for XL
+   * @param {Number} offsetXR - Number of offset for XR
+   */
   setHitbox(offsetYT = 0, offsetYB = 0, offsetXL = 0, offsetXR = 0) {
     this.hitbox = {
       active: true,
@@ -85,6 +99,9 @@ class DrawableObject {
     };
   }
 
+  /**
+   * Deleted the hitbox of object
+   */
   deletHitbox() {
     this.hitbox = {
       active: false,
